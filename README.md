32d7ba4c9e00b256d71cedfa2d1998f2f208bbeb
Curtis <curtis@tinbrain.net>
2012-09-24 23:36:27 +1000

    Updated README, added TODO

--- README.md
+++ README.md
@@ -2,3 +2,47 @@ formulation
 ===========
 
 Django Form rendering helper tags
+
+
+Usage
+=====
+
+First, write a template where each block is a way to render a field.
+
+We'll start with a simple one, with one hardy, general purpose field block.  Let's call it "mytemplate.form":
+
+    {% block field %}
+    {% if not nolabel %}
+    <label for="{{ field.auto_id }}" {% if field.required %}class="required"{% endif %}> {{ label|default:field.label }} </label>
+    {% endif %}
+    <input type="{{ field_type|default:"text" }}" name="{{ field.html_name }}" id="{{ field.auto_id }}" value="{{ field.value|default:"" }}" {% if field.errors %}class="error"{% endif %} />
+    {{ field.help_text }}
+    {% endblock %}
+
+Then, in your own template:
+
+    {% load formulation %}
+
+    <form method="POST" ... >
+    {% form "mytemplate.form" %}
+    {% field form.foo 'field' %}
+    {% field form.bar 'field' nolabel=True %}
+    {% field form.baz 'field' type='email' %}
+    {% endform %}
+
+Yep, it's that simple.
+
+Want more complex field types:
+
+    {% block TypedChoiceField %}
+    {% if not nolabel %}
+    <label for="{{ field.id }}" {% if field.required %}class="required"{% endif %}> {{ field.label }} </label>
+    {% endif %}
+    <select name="{{ field.html_name }}" id="{{ field.auto_id }}" {% if field.errors %}class="error"{% endif %}>
+    {% for option_value, option_label in field.field.choices %}
+    <option value="{{ option_value }}" {% if field.value == option_value|safe %}selected="selected"{% endif %}>{{ option_label }}</option>
+    {% endfor %}
+    </select>
+    {{ field.help_text }}
+    {% endblock %}
+
---
+++ formulation/templatetags/formulation.py
@@ -0,0 +1,93 @@
+
+from django import template
+from django.template.base import token_kwargs
+from django.template.loader import get_template
+from django.template.loader_tags import BlockNode, ExtendsNode, BlockContext
+
+register = template.Library()
+
+def resolve_blocks(template, context, blocks=None):
+    '''Get all the blocks from this template, accounting for 'extends' tags'''
+    if blocks is None:
+        blocks = BlockContext()
+
+    # If it's just the name, resolve into template
+    if isinstance(template, basestring):
+        template = get_template(template)
+
+    # Add this templates blocks as the first
+    local_blocks = dict(
+        (block.name, block)
+        for block in template.nodelist.get_nodes_by_type(BlockNode)
+    )
+    blocks.add_blocks(local_blocks)
+
+    # Do we extend a parent template?
+    extends = template.nodelist.get_nodes_by_type(ExtendsNode)
+    if extends:
+        # Can only have one extends in a template
+        extends_node = extends[0]
+
+        # Get the parent, and recurse
+        parent_template = extends_node.get_parent(context)
+        resolve_blocks(parent_template, context, blocks)
+
+    return blocks
+
+
+@register.tag
+def form(parser, token):
+    '''Open a context providing blocks from the named sniplate'''
+    bits = token.split_contents()
+    tag_name = bits.pop(0) # Remove the tag name
+    try:
+        tmpl_name = parser.compile_filter(bits.pop(0))
+    except IndexError:
+        raise template.TemplateSyntaxError("%r tag takes at least 1 argument: the widget template" % tag_name)
+
+    kwargs = token_kwargs(bits, parser)
+
+    nodelist = parser.parse(('endform',))
+    parser.delete_first_token()
+
+    return FormNode(tmpl_name, nodelist, kwargs)
+
+
+class FormNode(template.Node):
+    def __init__(self, tmpl_name, nodelist, kwargs
+        self.tmpl_name = tmpl_name
+        self.nodelist= nodelist
+        self.kwargs = kwargs
+
+    def render(self, context):
+        # Resolve our arguments
+        tmpl_name = self.tmpl_name.resolve(context)
+        kwargs = dict(
+            (key, val.resolve(context)
+            for key, val in self.kwargs.items()
+        )
+
+        # Grab the template snippets
+        kwargs['widgets'] = resolve_blocks(tmpl_name, context)
+
+        # Render our children
+        context.update(options)
+        output = self.nodelist.render(context)
+        context.pop()
+
+        return output
+
+
+@register.simpletag(takes_context=True)
+def field(context, field, widget, **kwargs):
+    kwargs['field'] = field
+    context.update(options)
+    output = context['widgets'].get_block(widget).render(context)
+    context.pop()
+
+    return output
+
+
+@register.simpletag(takes_context=True)
+def use(context, widget):
+    return context['widgets'].get_block(widget).render(context)
