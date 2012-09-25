3a6a6a2e67712e0ab39961a3b49d7a635a03de8b
Curtis <curtis@tinbrain.net>
2012-09-25 22:17:25 +1000

    Updated download URL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
     url='http://github.com/funkybob/formulation',
-    download_url='https://github.com/downloads/funkybob/formulation/formulation-1.0.1.tar.gz',
+    download_url='https://github.com/funkybob/formulation/tarball/1.0.2',
     keywords=['django', 'forms', 'templates'],
     packages = find_packages(),
     zip_safe=False,
