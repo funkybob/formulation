f59aefc7c9090faabd541921813e1ac01d90978f
Curtis <curtis@tinbrain.net>
2013-09-26 09:35:12 +1000

    Bumped version to v2.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='formulation',
-    version='2.0.2',
+    version='2.0.3',
     description='Django Form rendering tool',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
