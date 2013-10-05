a691722efebe8a50cc7b7a4f4af1e4c5af1a8260
Curtis Maloney <curtis@tinbrain.net>
2013-10-05 20:21:44 +1000

    Bumped version to 2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='formulation',
-    version='2.0.3',
+    version='2.0.4',
     description='Django Form rendering tool',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
