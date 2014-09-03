f10ae262ae475b0ae048efae4f41222bc5a99f92
funkybob <curtis@tinbrain.net>
2014-09-03 18:25:22 +1000

    Update test_inheritance to use setup_test_template_loader

--- tests/tests/test_inheritance.py
+++ tests/tests/test_inheritance.py
@@ -1,9 +1,39 @@
 from django.template import Context
 from django.template.loader import get_template
 from django.test import SimpleTestCase
+from django.test.utils import setup_test_template_loader, restore_template_loaders
+
+
+TEMPLATES = {
+    'formulation/default.form': '''
+''',
+    'base.html': '''
+{% block one %}<span>one</span>{% endblock %}
+{% block form %}{% endblock %}
+{% block two %}<div>two</div>{% endblock %}
+''',
+    'inherited.html': '''
+{% extends "base.html" %}
+{% load formulation %}
+{% block form %}
+    {% form 'formulation/default.form' %}
+        <form>empty</form>
+    {% endform %}
+{% endblock %}
+{% block two %}<div>overridden</div>{% endblock %}
+''',
+}
 
 
 class InheritanceTests(SimpleTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        setup_test_template_loader(TEMPLATES)
+
+    @classmethod
+    def tearDownClass(cls):
+        restore_template_loaders()
 
     def test_inheritance(self):
         """
